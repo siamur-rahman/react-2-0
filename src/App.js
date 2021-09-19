c89d3a0d8ec0cac2c49b5c43390299148a1b7dde
unknown <siamur01@gmail.com>
2021-09-19 22:14:09 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+
+
     </div>
   );
 }
+// person ekta function abar HTML tag
+function Person(props) {
+  console.log(props);
+  const person = {
+    backgroundColor: 'skyblue',
+    border: '5px solid lightsalmon',
+    margin: '20px',
+    borderRadious: '10 px'
+  }
+  return (
+    <div style={person}>
+      <h1>Name: Sakib All Hasan</h1>
+      <h4>Profession:Cricketer</h4>
+    </div>
 
+  );
+}
+
+
+
+
+function Friend() {
+  return (
+    <div className="person">
+      <h3>
+        Phone:
+      </h3>
+    </div>
+  )
+}
 export default App;
